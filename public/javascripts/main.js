44a7d81b0a391c9dbfcca400e8f3b9d9e61c53ca
Martin Autumn <martin@herbst-winter.net>
2014-05-06 15:31:28 +0200

    Z axle + round

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,13 +2,14 @@ function handleOrientation(event) {
 
 	var iosocket = io.connect();
 
-	var x = event.beta;
-	var y = event.gamma;
+	var z = Math.floor(event.alpha);
+	var x = Math.floor(event.beta);
+	var y = Math.floor(event.gamma);
 	
 	document.getElementById("x").innerHTML = x;
 	document.getElementById("y").innerHTML = y;
 	
-	iosocket.emit('data', x + " " + y);
+	iosocket.emit('data', x + " " + y + " " + z);
 	
 	console.log('orientation handler fires');
 }
