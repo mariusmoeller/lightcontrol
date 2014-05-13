2bcf9a2efd48228a82dee453a8cea91099fba919
Martin Autumn <martin@herbst-winter.net>
2014-05-13 15:12:50 +0200

    Add colorwheel

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -5,13 +5,28 @@ function handleOrientation(event) {
 	var z = event.alpha;
 	var x = event.beta;
 	var y = event.gamma;
-	
+
 	var orientation = [x, y, z];
-	
+
 	document.getElementById("x").innerHTML = x;
 	document.getElementById("y").innerHTML = y;
-	
+
 	socket.emit('data', orientation);
 }
+
+
+window.onload = function() {
+	$.minicolors.defaults = $.extend($.minicolors.defaults, {
+		changeDelay: 200,
+		letterCase: 'uppercase',
+		theme: 'wheel',
+		inline: true,
+		control: 'wheel'
+	});
+
+	$('#colors').minicolors();
+
+}
+
 
 window.addEventListener('deviceorientation', handleOrientation);
