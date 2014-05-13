24c5c64f521f5a71857b8923368be238e84a5722
Martin Autumn <martin@herbst-winter.net>
2014-05-13 19:44:40 +0200

    Add route + button

--- app.js
+++ app.js
@@ -6,6 +6,7 @@
 var express = require('express');
 var routes = require('./routes');
 var user = require('./routes/user');
+var program = require('./routes/program');
 var http = require('http');
 var path = require('path');
 var socketio = require('socket.io');
@@ -39,6 +40,7 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.get('/programs', program.list);
 
 var server = http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
