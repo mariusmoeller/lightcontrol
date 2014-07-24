b0cfe53e63abeda1c39b468879240a2bc5d9d6f5
anna <anna.muenster@web.de>
2014-07-24 12:19:00 +0200

    fixed dimensions, added thumbnails and highscore

---
+++ public/javascripts/highscore.js
@@ -0,0 +1,30 @@
+var highscore = {
+	"easy": [
+		{name: "Anna", score: 1234},
+		{name: "Bernie", score: 900},
+		{name: "Anro", score: 878},
+		{name: "Simon", score: 612},
+		{name: "Michel", score: 400}
+	],
+	"medium":[
+		{name: "Anna", score: 1234},
+		{name: "Bernie", score: 900},
+		{name: "Anro", score: 878},
+		{name: "Simon", score: 612},
+		{name: "Michel", score: 400}
+	],
+	"hard": [
+		{name: "Anna", score: 1234},
+		{name: "Bernie", score: 900},
+		{name: "Anro", score: 878},
+		{name: "Simon", score: 612},
+		{name: "Michel", score: 400}
+	],
+	"extreme": [
+		{name: "Anna", score: 1234},
+		{name: "Bernie", score: 900},
+		{name: "Anro", score: 878},
+		{name: "Simon", score: 612},
+		{name: "Michel", score: 400}
+	]
+};
